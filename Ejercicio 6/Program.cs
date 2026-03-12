140b19a155c6f6b118f8b8c9206bbb1feec05665
verno <verno@ASUS>
2026-03-11 22:04:54 -0600

    ej 6 finalizado

--- Ejercicio 6/Program.cs
+++ Ejercicio 6/Program.cs
@@ -1,8 +1,13 @@
 ﻿Console.WriteLine("Crear una función que reciba un número en grados Celsius y devuelva el valor convertido a Fahrenheit.");
 
-double ConvertirFahrenheit (int gradoscel)
+double ConvertirFahrenheit (double gradoscel)
 {
     double Fahrenheit = (gradoscel * 9 / 5) + 32;
     return Fahrenheit;
 }
 
+Console.WriteLine("Ingrese grados celsius");
+double num1 = double.Parse(Console.ReadLine());
+
+double grados = ConvertirFahrenheit(num1);
+Console.WriteLine($"Los grados fh son {grados}");
