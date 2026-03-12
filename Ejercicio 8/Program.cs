5b8de39652d16f275f1314c587598955a969d7a3
verno <verno@ASUS>
2026-03-12 00:22:00 -0600

    funcion creada ej 8

---
+++ Ejercicio 8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Crear una función que reciba el salario por hora y las horas trabajadas y devuelva el salario total.");
+
+int SalarioFinal (int salahora, int horatraba)
+{
+    return salahora * horatraba;
+}
+
