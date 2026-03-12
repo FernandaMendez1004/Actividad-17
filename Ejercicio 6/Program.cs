8da71025d31d41a6ccead6ea0c62c6c85df7dfb4
verno <verno@ASUS>
2026-03-11 22:00:26 -0600

    funcion ej 6

---
+++ Ejercicio 6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Crear una función que reciba un número en grados Celsius y devuelva el valor convertido a Fahrenheit.");
+
+double ConvertirFahrenheit (int gradoscel)
+{
+    double Fahrenheit = (gradoscel * 9 / 5) + 32;
+    return Fahrenheit;
+}
+
