181ec830664f3c1cac413730b09d97646c27a697
verno <verno@ASUS>
2026-03-12 00:39:03 -0600

    funcion creada ej 9

---
+++ Ejercicio 9/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Crear una función que reciba tres números y devuelva el promedio de los tres.");
+
+double PromedioTres (double a, double b, double c)
+{
+    return (a + b + c) / 3;
+}
+
