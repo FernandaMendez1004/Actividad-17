e4b3f62d001d524b4fcaed6443a7c0191acb0f7f
verno <verno@ASUS>
2026-03-11 20:36:17 -0600

    pedir numeros

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -3,8 +3,11 @@
     return a + b;
 }
 
-
-
 Console.WriteLine("Ingrese un numero");
-int resultado = Sumar 
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese otro numero");
+int n2 = int.Parse(Console.ReadLine());
+
+int resultado = Sumar(int n1, int n2);
+
 
