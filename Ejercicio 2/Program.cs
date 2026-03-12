6a64dba9dfd57f4f58126fc94268bed6da3b62bd
verno <verno@ASUS>
2026-03-11 20:49:07 -0600

    ej 2 terminado cuadrado

---
+++ Ejercicio 2/Program.cs
@@ -0,0 +1,10 @@
+﻿int Cuadrado (int a)
+{
+    return a * a;
+}
+
+Console.WriteLine("Ingrese un numero para elevar al cuadrado");
+int n1 = int.Parse(Console.ReadLine());
+
+int resultado = Cuadrado(n1);
+Console.WriteLine($"El resultado es {resultado}");
