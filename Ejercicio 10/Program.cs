4f8ea0442cba4fb16a7510ea6025e93a096d5fd7
verno <verno@ASUS>
2026-03-12 00:47:16 -0600

    imprimir y pedir num del ej 10

--- Ejercicio 10/Program.cs
+++ Ejercicio 10/Program.cs
@@ -4,3 +4,9 @@ int CuboNumero (int a)
 {
     return a * a * a;
 }
+
+Console.WriteLine("Ingrese un numero para calcular el cubo");
+int n1 = int.Parse(Console.ReadLine());
+
+int cuboresu = CuboNumero(n1);
+Console.WriteLine($"El cubo es {cuboresu}");
