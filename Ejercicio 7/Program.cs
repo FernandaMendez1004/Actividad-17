26c2366331adf57719b069d7fc7413d3faeecb7e
verno <verno@ASUS>
2026-03-12 00:12:18 -0600

    funcion ej 7

---
+++ Ejercicio 7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Crear una función que reciba el precio de un producto y el porcentaje de descuento y devuelva el precio final.");
+
+double PrecioFinal (double precio, double descuento)
+{
+    double resu = precio - (precio * descuento / 100);
+    return resu;
+}
+
