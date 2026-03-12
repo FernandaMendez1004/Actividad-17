836460b16b78484cdae43ce714ac24a47c92dfda
verno <verno@ASUS>
2026-03-12 00:41:59 -0600

    promedio de 3 ej 9

--- Ejercicio 9/Program.cs
+++ Ejercicio 9/Program.cs
@@ -5,3 +5,12 @@ double PromedioTres (double a, double b, double c)
     return (a + b + c) / 3;
 }
 
+Console.WriteLine("Ingrese el primer numero");
+double n1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+double n2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el terer numero");
+double n3 = double.Parse(Console.ReadLine());
+
+double prom = PromedioTres(n1, n2, n3);
+Console.WriteLine($"El promedio es de {prom}");
