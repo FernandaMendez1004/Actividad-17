cbe21991fa16d52d70f9cf05d1edcbd348753abd
verno <verno@ASUS>
2026-03-11 21:07:34 -0600

    funcion arreglada ej 4

--- Ejercicio 4/Program.cs
+++ Ejercicio 4/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("Crear una función que reciba el radio de un círculo y devuelva el área del círculo.");
-int RadioCirculo (int a)
+double AreaCirculo (double a)
 {
-
+    double area = a * a * 3.1416;
+    return area;
 }
+
