12802794d320be8e8f5af89422f9e6717fbfbb38
verno <verno@ASUS>
2026-03-11 21:19:17 -0600

    ej funcion terminada

---
+++ Ejercicio 5/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Crear una función que reciba la base y la altura de un rectángulo y devuelva su área.");
+int AreaRectangulo (int basec, int alturac)
+{
+    int area = basec * alturac;
+    return area;
+}
